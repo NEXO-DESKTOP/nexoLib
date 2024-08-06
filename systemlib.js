5b815d9568369bea0e04014c7b21b745e266a978
lucasengithub <86058446+lucasengithub@users.noreply.github.com>
2024-08-06 02:02:16 +0200

    al menos tiene algo

---
+++ systemlib.js
@@ -0,0 +1,61 @@
+Neutralino.init()
+
+function onWindowClose() {
+    Neutralino.app.exit();
+}
+
+window.addEventListener('resize', adjustLeftSector);
+
+function adjustLeftSector() {
+  const centerSector = document.getElementById('windowCenterSector');
+  const leftSector = document.getElementById('windowLeftSector');
+  const rightSector = document.getElementById('windowRightSector');
+  
+  const centerSectorWidth = centerSector.offsetWidth;
+  const halfCenterWidth = centerSectorWidth / 2;
+  
+  leftSector.style.right = `calc(50% + ${halfCenterWidth}px)`;
+  rightSector.style.left = `calc(50% + ${halfCenterWidth}px)`;
+}
+
+// Llama a la función inicialmente para ajustar el tamaño al cargar la página
+adjustLeftSector();
+
+async function resizemain() {
+    let status = await Neutralino.window.isMaximized();
+    if (!status) {
+        document.getElementById('resbtn').innerHTML = '<span class="sIcons">close_fullscreen</span>';
+        Neutralino.window.maximize();
+        
+    } else {
+        Neutralino.window.unmaximize();
+        document.getElementById('resbtn').innerHTML = '<span class="sIcons">open_in_full</span>';
+    }
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    Neutralino.window.setDraggableRegion('appBar');
+});
+
+
+document.getElementById('closebtn').addEventListener('click', () => {
+    Neutralino.app.exit();
+  });
+  
+  document.getElementById('minbtn').addEventListener('click', () => {
+      Neutralino.window.minimize();
+      console.log('minimize');
+  });
+
+  document.getElementById('resbtn').addEventListener('click', () => {resizemain()});
+
+  document.getElementById('appBar').addEventListener('dblclick', () => {resizemain()});
+
+
+
+
+// document.addEventListener('contextmenu', (e) => {
+//     e.preventDefault(); 
+//});
+
